6e0b246b97e83dacb427741d3f9c3327ac02cff4
Yihan Lin <lllllyh01@users.noreply.github.com>
2023-05-04 20:17:22 +0800

    init imgcap_system

---
+++ frontend/src/components/Person.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>用户名：lyh</h3>
+    <el-button class="modify" @click="modify_username">修改</el-button>
+</template>
+
+<script>
+    export default {
+        name: 'Person',
+        data () {
+            return {
+                username: "",
+                passwd: "",
+                gender: "",
+                age: ""
+            }
+        },
+        methods: {
+            modify_username() {
+                console.log("in modify_username");
+            }
+        },
+    }
+</script>
