66bfa595020fa6788167a6f5a8f1b595bbc82f01
Yihan Lin <lllllyh01@users.noreply.github.com>
2023-11-07 17:29:30 -0500

    make this repository only for model

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
-import router from './router'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-const app = createApp(App).use(router)
-installElementPlus(app)
-app.mount('#app')
